8980e895feb3272f8640a92cb0b95066a0f5763c
Tiffany Gallagher <info@gallaghercollectiveworks.com>
2026-04-14 19:11:49 -0400

    Fix: add missing useEffect and useState imports to Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Users, UtensilsCrossed, Truck, Timer, Clock, FileText, Calendar, Headphones, FolderOpen, Shield } from 'lucide-react'
 import GCWNav from '../components/GCWNav'
