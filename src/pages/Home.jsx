63dc578a0f3a17a12c143f0a73ba9e9cb197b88f
Tiffany Gallagher <info@gallaghercollectiveworks.com>
2026-04-14 19:17:42 -0400

    Fix: properly deduplicate React imports in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Users, UtensilsCrossed, Truck, Timer, Clock, FileText, Calendar, Headphones, FolderOpen, Shield } from 'lucide-react'
 import GCWNav from '../components/GCWNav'
